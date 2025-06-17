6f49374b1ecb578cb35eb2917d4513913f5fe90b
pr401n <pr401n@gmail.com>
2025-06-17 11:15:59 -0700

    Update flight_agent.py

--- flight_agent.py
+++ flight_agent.py
@@ -2,7 +2,4 @@ from dotenv import load_dotenv
 from typing import Annotated, Literal
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
-from typing_extensions import TypedDict
-from langchain_google_genai import ChatGoogleGenerativeAI
+
